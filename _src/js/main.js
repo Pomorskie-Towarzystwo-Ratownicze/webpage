75148670ffa669349f1c40d04617c36929e5a657
Mateusz Iwaniec <mateusz.iwaniec@qvistorp.com>
2024-07-19 11:32:02 +0200

    Change dir nam

--- _src/js/main.js
+++ _src/js/main.js
@@ -81,3 +81,14 @@
 
     });
 }(jQuery));
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
